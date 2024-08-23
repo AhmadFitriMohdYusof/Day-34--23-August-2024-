da4418ab7f9c857aa1528c07f547a23b782af338
AhmadFitriMohdYusof <ahmadfitriyusof1997@gmail.com>
2024-08-23 11:29:42 +0800

    first commit

---
+++ assignment.py
@@ -0,0 +1,25 @@
+def greet_user(name, greeting="Hello"):
+   
+    print(f"{greeting}, {name}!")
+
+greet_user("Fitri")              
+greet_user("Zayn", "Good morning") 
+
+
+def sum_numbers(a, b):
+    
+    return a + b
+result = sum_numbers(7, 13) 
+print(result)               
+
+
+fruits = ["apple", "banana", "cherry", "date"]
+fruits.append("elderberry")
+fruits.remove("banana")
+fruits.insert(1, "blueberry")
+fruits.sort()
+print(fruits) 
+
+
+
+
